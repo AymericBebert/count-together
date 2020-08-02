4faac8710267014e1c405eee84aa4017af5085b6
AymericBebert <aymeric.bernard@hotmail.fr>
2020-08-02 18:22:48 +0200

    Better (& no conflict) wheel animations

--- src/app/random-wheel/wheel/wheel.component.ts
+++ src/app/random-wheel/wheel/wheel.component.ts
@@ -10,12 +10,12 @@ import {
   SimpleChanges,
   ViewChild
 } from '@angular/core';
-import {Subject} from 'rxjs';
+import {BehaviorSubject, Subject} from 'rxjs';
 import {debounceTime} from 'rxjs/operators';
 import {interpolateString} from 'd3-interpolate';
-import {select} from 'd3-selection';
+import {select, Selection} from 'd3-selection';
 import {scaleLinear} from 'd3-scale';
-import {curveLinearClosed, lineRadial, line} from 'd3-shape';
+import {curveLinearClosed, line, lineRadial} from 'd3-shape';
 import 'd3-transition';
 
 @Component({
@@ -31,20 +31,24 @@ export class WheelComponent implements OnInit, OnDestroy, OnChanges, AfterViewIn
 
   private oldAngle = 180;
   private angle = 180;
-  private spinning = false;
+  private arrowPrepared = false;
 
+  private spinning$ = new BehaviorSubject<boolean>(false);
   private destroy$ = new Subject<void>();
 
   public readonly fontSize = '14px';
   private readonly outerRadiusMargin = 40;
   private readonly redrawDuration = 500;
-  private readonly spinDuration = 3000;
+  private readonly spinDuration = 4000;
 
   @ViewChild('svgRef', {static: true}) public svgRef: ElementRef<SVGElement>;
   private width: number;
-  private height: number;
   private middle: number;
   private far: number;
+
+  private arrowWrapper: Selection<any, unknown, null, undefined>;
+  private arrowRotate: Selection<any, unknown, null, undefined>;
+  private arrowInner: Selection<any, unknown, null, undefined>;
 
   private onResize$ = new Subject<void>();
 
@@ -65,7 +69,6 @@ export class WheelComponent implements OnInit, OnDestroy, OnChanges, AfterViewIn
   }
 
   ngOnInit(): void {
-    this.prepareDraw();
   }
 
   ngOnDestroy(): void {
@@ -77,8 +80,9 @@ export class WheelComponent implements OnInit, OnDestroy, OnChanges, AfterViewIn
     this.onResize$
       .pipe(debounceTime(100))
       .subscribe(() => {
-        this.prepareDraw();
-        this.draw();
+        this.updateSizes();
+        this.adjustZones();
+        this.adjustArrowPosition();
       });
 
     if (this.names.length > 0) {
@@ -87,53 +91,52 @@ export class WheelComponent implements OnInit, OnDestroy, OnChanges, AfterViewIn
       this.names = new Array(this.nb).fill('').map((_, i) => '' + (i + 1));
     }
 
-    this.prepareDraw();
-    this.draw();
+    this.prepareArrow();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.prepareDraw();
+    if (!this.arrowPrepared) {
+      this.updateSizes();
+      this.arrowPrepared = true;
+    }
     if (changes.names?.currentValue) {
       this.nb = this.names.length;
-      this.draw();
+      this.adjustZones();
       return;
     }
     if (changes.nb?.currentValue) {
       this.names = new Array(this.nb).fill('').map((_, i) => '' + (i + 1));
-      this.draw();
+      this.adjustZones();
       return;
     }
     if (changes.dark) {
-      this.draw();
+      this.adjustZones();
+      this.adjustArrowColor();
       return;
     }
   }
 
   public spin() {
-    if (!this.spinning) {
+    if (!this.spinning$.getValue()) {
       this.angle = this.angle + 360 * 3 + Math.random() * 360;
-      this.draw();
-      this.spinning = true;
+      this.adjustArrowRotate();
       setTimeout(() => {
-        this.spinning = false;
         this.oldAngle = this.angle;
       }, this.spinDuration);
     }
   }
 
-  private prepareDraw() {
+  private updateSizes() {
     this.width = this.svgRef.nativeElement.getBoundingClientRect().width;
-    // noinspection JSSuspiciousNameCombination
-    this.height = this.width;
 
     this.middle = this.width / 2;
     this.far = this.middle - this.outerRadiusMargin;
 
     select(this.svgRef.nativeElement)
-      .attr('height', this.height);
+      .attr('height', this.width);
   }
 
-  private draw() {
+  private adjustZones() {
     const radianAngleScale = scaleLinear<number, number>()
       .domain([0, this.names.length])
       .range([-Math.PI, Math.PI]);
@@ -157,8 +160,9 @@ export class WheelComponent implements OnInit, OnDestroy, OnChanges, AfterViewIn
 
     const labelsEnter = labels.enter()
       .append('g')
+      .attr('class', 'label')
       .attr('transform', centerTransformFn)
-      .attr('class', 'label');
+      .style('opacity', 1);
 
     const labelPathDFn = (d, i) => radarLine([[0, 0], [1, i + 0.5]]);
     const labelColorFn = this.dark ? '#cccccc' : '#444444';
@@ -169,11 +173,7 @@ export class WheelComponent implements OnInit, OnDestroy, OnChanges, AfterViewIn
       .attr('opacity', 0.25)
       .attr('stroke-width', 2);
 
-    const labelTextXFn = (d, i) => {
-      return Math.sin(radianAngleScale(i)) * (radiusScale(0.85) + this.outerRadiusMargin * 0.45);
-      // const ratio = Math.sin(radianAngleScale(i));
-      // return (ratio - 0.1 * ratio * ratio * ratio) * (radiusScale(maxValue) + this.outerRadiusMargin * 0.45);
-    };
+    const labelTextXFn = (d, i) => Math.sin(radianAngleScale(i)) * (radiusScale(0.85) + this.outerRadiusMargin * 0.45);
     const labelTextYFn = (d, i) => -Math.cos(radianAngleScale(i)) * (radiusScale(0.85) + this.outerRadiusMargin * 0.3);
 
     labelsEnter.append('text')
@@ -183,7 +183,6 @@ export class WheelComponent implements OnInit, OnDestroy, OnChanges, AfterViewIn
       .attr('text-anchor', 'middle')
       .style('font-size', this.fontSize)
       .style('fill', labelColorFn)
-      // .style('text-shadow', '0 0 10px white, 0 0 10px white')
       .text(d => d);
 
     const labelsUpdate = labelsEnter.merge(labels)
@@ -191,7 +190,8 @@ export class WheelComponent implements OnInit, OnDestroy, OnChanges, AfterViewIn
       .duration(this.redrawDuration);
 
     labelsUpdate
-      .attr('transform', centerTransformFn);
+      .attr('transform', centerTransformFn)
+      .style('opacity', 1);
 
     labelsUpdate.select('text')
       .attr('x', labelTextXFn)
@@ -203,149 +203,60 @@ export class WheelComponent implements OnInit, OnDestroy, OnChanges, AfterViewIn
       .attr('d', labelPathDFn)
       .attr('stroke', labelColorFn);
 
-    labels.exit().remove();
-
-    // ----- ARROW -----
-
-    const arrows = select(this.svgRef.nativeElement).select('.arrows')
-      .selectAll<SVGGElement, string>('.arrow')
-      .data([this.angle]);
-
-    const arrowsEnter = arrows.enter()
-      .append('g')
-      .attr('transform', centerTransformFn)
-      .attr('class', 'arrow');
-
-    const arrowTransformFn = `rotate(${this.angle}) scale(${this.far})`;
-    const arrowColorFn = this.dark ? '#6648be' : '#2b1963';
-
-    arrowsEnter.append('path')
-      .attr('d', this.arrowPathDFn(this.arrowPathData))
-      .attr('transform', arrowTransformFn)
-      .attr('fill', arrowColorFn);
-
-    const arrowsUpdate = arrowsEnter.merge(arrows)
+    labels
+      .exit()
       .transition()
-      .duration(this.spinDuration);
-
-    arrowsUpdate
-      .attr('transform', centerTransformFn);
-
-    arrowsUpdate.select('path')
-      .attrTween('transform', () => interpolateString(
-        `rotate(${this.oldAngle}) scale(${this.far})`,
-        `rotate(${this.angle}) scale(${this.far})`,
-      ))
-      .attr('fill', arrowColorFn);
-
-    arrows.exit().remove();
+      .duration(this.redrawDuration)
+      .style('opacity', 0)
+      .attr('transform', `${centerTransformFn} scale(0.2)`)
+      .remove();
   }
 
-  // private drawSpin() {
-  //   const radianAngleScale = scaleLinear<number, number>()
-  //     .domain([0, this.names.length])
-  //     .range([-Math.PI, Math.PI]);
-  //
-  //   const radiusScale = scaleLinear<number, number>()
-  //     .domain([0, 1])
-  //     .range([0, this.far]);
-  //
-  //   const radarLine = lineRadial<[number, number]>()
-  //     .curve(curveLinearClosed)
-  //     .radius(d => radiusScale(d[0]))
-  //     .angle(d => radianAngleScale(d[1]));
-  //
-  //   const centerTransformFn = `translate(${this.middle} ${this.middle})`;
-  //
-  //   // ----- SEPARATIONS -----
-  //
-  //   const labels = select(this.svgRef.nativeElement).select('.labels')
-  //     .selectAll<SVGGElement, string>('.label')
-  //     .data(this.names);
-  //
-  //   const labelsEnter = labels.enter()
-  //     .append('g')
-  //     .attr('transform', centerTransformFn)
-  //     .attr('class', 'label');
-  //
-  //   const labelPathDFn = (d, i) => radarLine([[0, 0], [1, i + 0.5]]);
-  //   const labelColorFn = this.dark ? '#cccccc' : '#444444';
-  //
-  //   labelsEnter.append('path')
-  //     .attr('d', labelPathDFn)
-  //     .attr('stroke', labelColorFn)
-  //     .attr('opacity', 0.25)
-  //     .attr('stroke-width', 2);
-  //
-  //   const labelTextXFn = (d, i) => {
-  //     return Math.sin(radianAngleScale(i)) * (radiusScale(0.85) + this.outerRadiusMargin * 0.45);
-  //     // const ratio = Math.sin(radianAngleScale(i));
-  //     // return (ratio - 0.1 * ratio * ratio * ratio) * (radiusScale(maxValue) + this.outerRadiusMargin * 0.45);
-  //   };
-  //   const labelTextYFn = (d, i) => -Math.cos(radianAngleScale(i)) * (radiusScale(0.85) + this.outerRadiusMargin * 0.3);
-  //
-  //   labelsEnter.append('text')
-  //     .attr('x', labelTextXFn)
-  //     .attr('y', labelTextYFn)
-  //     .attr('alignment-baseline', 'middle')
-  //     .attr('text-anchor', 'middle')
-  //     .style('font-size', this.fontSize)
-  //     .style('fill', labelColorFn)
-  //     // .style('text-shadow', '0 0 10px white, 0 0 10px white')
-  //     .text(d => d);
-  //
-  //   const labelsUpdate = labelsEnter.merge(labels)
-  //     .transition()
-  //     .duration(this.redrawDuration);
-  //
-  //   labelsUpdate
-  //     .attr('transform', centerTransformFn);
-  //
-  //   labelsUpdate.select('text')
-  //     .attr('x', labelTextXFn)
-  //     .attr('y', labelTextYFn)
-  //     .style('fill', labelColorFn)
-  //     .text(d => d);
-  //
-  //   labelsUpdate.select('path')
-  //     .attr('d', labelPathDFn)
-  //     .attr('stroke', labelColorFn);
-  //
-  //   labels.exit().remove();
-  //
-  //   // ----- ARROW -----
-  //
-  //   const arrows = select(this.svgRef.nativeElement).select('.arrows')
-  //     .selectAll<SVGGElement, string>('.arrow')
-  //     .data([this.angle]);
-  //
-  //   const arrowsEnter = arrows.enter()
-  //     .append('g')
-  //     .attr('transform', centerTransformFn)
-  //     .attr('class', 'arrow');
-  //
-  //   const arrowTransformFn = `rotate(${this.angle}) scale(${this.far})`;
-  //   const arrowColorFn = this.dark ? '#6648be' : '#2b1963';
-  //
-  //   arrowsEnter.append('path')
-  //     .attr('d', this.arrowPathDFn(this.arrowPathData))
-  //     .attr('transform', arrowTransformFn)
-  //     .attr('fill', arrowColorFn);
-  //
-  //   const arrowsUpdate = arrowsEnter.merge(arrows)
-  //     .transition()
-  //     .duration(this.spinDuration);
-  //
-  //   arrowsUpdate
-  //     .attr('transform', centerTransformFn);
-  //
-  //   arrowsUpdate.select('path')
-  //     .attrTween('transform', () => interpolateString(
-  //       `rotate(${this.oldAngle}) scale(${this.far})`,
-  //       `rotate(${this.angle}) scale(${this.far})`,
-  //     ))
-  //     .attr('fill', arrowColorFn);
-  //
-  //   arrows.exit().remove();
-  // }
+  private prepareArrow() {
+    this.arrowWrapper = select(this.svgRef.nativeElement).select('.arrow-wrapper');
+    this.arrowRotate = select(this.svgRef.nativeElement).select('.arrow-rotate');
+    this.arrowInner = select(this.svgRef.nativeElement).select('.arrow-inner');
+
+    this.arrowWrapper
+      .attr('transform', `translate(${this.middle} ${this.middle}) scale(${this.far})`);
+
+    this.arrowRotate
+      .attr('class', 'arrow-rotate')
+      .attr('transform', `rotate(${this.angle})`);
+
+    this.arrowInner
+      .attr('class', 'arrow-inner')
+      .attr('d', this.arrowPathDFn(this.arrowPathData))
+      .attr('fill', this.dark ? '#6648be' : '#2b1963');
+  }
+
+  private adjustArrowPosition() {
+    const arrowWrapper = select(this.svgRef.nativeElement).select('.arrow-wrapper');
+
+    arrowWrapper
+      .transition()
+      .duration(this.redrawDuration)
+      .attr('transform', `translate(${this.middle} ${this.middle}) scale(${this.far})`);
+  }
+
+  private adjustArrowRotate() {
+    const arrowRotate = select(this.svgRef.nativeElement).select('.arrow-rotate');
+
+    arrowRotate
+      .transition()
+      .duration(this.spinDuration)
+      .attrTween('transform', () => interpolateString(
+        `rotate(${this.oldAngle})`,
+        `rotate(${this.angle})`,
+      ));
+  }
+
+  private adjustArrowColor() {
+    const arrowInner = select(this.svgRef.nativeElement).select('.arrow-inner');
+
+    arrowInner
+      .transition()
+      .duration(this.redrawDuration)
+      .attr('fill', this.dark ? '#6648be' : '#2b1963');
+  }
 }

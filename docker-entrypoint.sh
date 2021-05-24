73592afc26b6ac480176f7e2c7af20b1462c63c1
AymericBebert <aymeric.bernard@hotmail.fr>
2021-05-24 09:51:29 +0200

    config directly in html

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,21 +2,30 @@
 
 set -eu
 
-echo "window['APP_CONFIG'] = {" >/usr/share/nginx/html/assets/config.js
-echo "  \"version\": \"$(cat /version.txt)\"," >>/usr/share/nginx/html/assets/config.js
+echo "window['APP_CONFIG'] = {" >/config.js
+echo "  \"version\": \"$(cat /version.txt)\"," >>/config.js
 
 env | while IFS= read -r line; do
   name=${line%%=*}
   value=${line#*=}
   case $name in NGX_*)
-    echo "  \"$(echo "$name" | cut -c5-)\": $value," >>/usr/share/nginx/html/assets/config.js
+    echo "  \"$(echo "$name" | cut -c5-)\": $value," >>/config.js
     ;;
   esac
 done
 
-echo "};" >>/usr/share/nginx/html/assets/config.js
+echo "};" >>/config.js
 
 sed -i "s/CONFIG_TIMESTAMP/$(date +%s)/" /usr/share/nginx/html/index.html
+
+CONFIG_BLOCK="<!--APP_CONFIG INSERTED-->
+<script>
+$(cat /config.js)
+</script>"
+NGX_GTAG=$(echo "$NGX_GTAG" | sed -e 's/^"//' -e 's/"$//')
+GTAG_BLOCK=$(echo "$GTAG_BLOCK" | sed -e "s/NGX_GTAG/$NGX_GTAG/")
+# shellcheck disable=SC2005
+echo "$(awk -v r="$CONFIG_BLOCK" '{gsub(/<!--APP_CONFIG-->/,r)}1' /usr/share/nginx/html/index.html)" >/usr/share/nginx/html/index.html
 
 if [ -n "${APP_SHORT_NAME:-}" ]; then
   sed -i "s/\"short_name\": \"CounT\"/\"short_name\": \"$APP_SHORT_NAME\"/" /usr/share/nginx/html/manifest.webmanifest
